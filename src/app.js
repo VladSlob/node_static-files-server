0514392bbd298a028668cd4931a263a5734c2c60
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-09-23 11:24:09 +0300

    app.js

--- src/app.js
+++ src/app.js
@@ -6,13 +6,6 @@ const axios = require('axios');
 const PORT = process.env.PORT || 5701;
 const server = createServer();
 
-axios
-  .get(`http://localhost:${PORT}/file`)
-  .then((response) => {
-    console.log('Server responded:', response.data);
-  })
-  .catch((error) => {
-    console.error('Error connecting to server:', error.message);
-  });
+
 
 server.listen(PORT);
