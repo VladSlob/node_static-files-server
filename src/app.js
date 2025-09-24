c8cfdcaa21641731aaed2f98dc995355c79d831c
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-09-24 11:07:55 +0300

    app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 /* eslint-disable no-console */
-'use strict';
 
-const { createServer } = require('./createServer');
-const axios = require('axios');
 const PORT = process.env.PORT || 5701;
 const server = createServer();
 
